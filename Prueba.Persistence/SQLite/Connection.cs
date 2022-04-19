0320c86fbcf73a44886d7e55e592e2de808a0d5a
Walter Leturia <leturia@live.com>
2022-04-18 22:04:02 -0500

    Persistence Layer integration

---
+++ Prueba.Persistence/SQLite/Connection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Prueba.Persistence
+{
+    public class Connection
+    {
+        private const string DBName = "database.sqlite";
+        private static bool IsDbRecentlyCreated = false;
+
+        public static void Up()
+        {
+            // Crea la base de datos y registra usuario solo una vez
+            if (!File.Exists(Path.GetFullPath(DBName)))
+            {
+                SQLiteConnection.CreateFile(DBName);
+                IsDbRecentlyCreated = true;
+            }
+
+            using var ctx = GetInstance();
+            if (IsDbRecentlyCreated)
+            {
+                string query = @"CREATE TABLE IF NOT EXISTS products 
+                        (id INTEGER PRIMARY KEY AUTOINCREMENT, code VARCHAR(20) NOT NULL UNIQUE, name VARCHAR(50) NOT NULL);";
+
+                using var command = new SQLiteCommand(query, ctx);
+                command.ExecuteNonQuery();
+
+            }
+        }
+
+        public static SQLiteConnection GetInstance()
+        {
+            var db = new SQLiteConnection(
+                string.Format("Data Source={0};Version=3;", DBName)
+            );
+
+            db.Open();
+            return db;
+        }
+    }
+}
---
+++ Prueba.Persistence/SQLite/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace Prueba.Persistence.SQLite
+{
+    public class Product : Domain.Repository.IProductRepository
+    {
+        readonly SQLiteConnection db;
+        public Product(SQLiteConnection db)
+        {
+            this.db = db;
+        }
+
+        public void Edit(Domain.Entity.Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Domain.Entity.Product> GetAll()
+        {
+            var result = new List<Domain.Entity.Product>();
+            var query = "SELECT * FROM products";
+            using var command = new SQLiteCommand(query, db);
+            using var reader = command.ExecuteReader();
+            if (!reader.HasRows)
+                return result;
+
+            while (reader.Read())
+            {
+                var builder = new Domain.Builder.ProductBuilder();
+                builder.SetCode(reader["Code"].ToString());
+                builder.SetId(Convert.ToInt32(reader["id"].ToString()));
+                builder.SetName(reader["Name"].ToString());
+                // Consultar caché
+                // Consultar servicio externo
+                result.Add(builder.GetProduct());
+            }
+            return result;
+        }
+
+        public Domain.Entity.Product GetById(int productId)
+        {
+            var query = $"SELECT * FROM products p WHERE p.id = {productId}";
+            using var command = new SQLiteCommand(query, db);
+            using var reader = command.ExecuteReader();
+            if (!reader.HasRows)
+                throw new Exception($"The id {productId} has no coincidences");
+
+            var result = new Domain.Entity.Product();
+            while (reader.Read())
+            {
+                result.Id = Convert.ToInt32(reader["id"].ToString());
+                result.Name = reader["Name"].ToString();
+                result.Code = reader["Code"].ToString();
+                // Consultar caché
+                // Consultar servicio externo
+            }
+            return result;
+        }
+
+        public void Save(Domain.Entity.Product product)
+        {
+            var query = $"INSERT INTO products VALUES (null, '{product.Code}', '{product.Name}')";
+            using var command = new SQLiteCommand(query, db);
+            var rows = command.ExecuteNonQuery();
+            if (rows != 1)
+                throw new Exception($"The insertion has affected {rows} rows");
+        }
+    }
+}
