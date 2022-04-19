6a766e6a4f3e93fd87c075a6000107719ba6876c
Walter Leturia <leturia@live.com>
2022-04-18 22:04:29 -0500

    Delivery http layer

---
+++ Prueba.Delivery/Controllers/Product.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Prueba.Delivery.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Product : ControllerBase
+    {
+        private readonly Domain.Usecase.IProductUsecase _productUC;
+
+        public Product(Domain.Usecase.IProductUsecase product)
+        {
+            _productUC = product;
+        }
+
+        [HttpGet]
+        public IEnumerable<Domain.VO.Product> List()
+        {
+            var (res, err) = _productUC.List();
+            if (err != null)
+                throw new ApplicationException(err.Message);
+            return res;
+        }
+
+        [HttpPost]
+        public StatusCodeResult Save([FromBody] Domain.DTO.ProductSave product)
+        {
+            var err = _productUC.Save(product);
+            if (err != null)
+                throw new ApplicationException(err.Message);
+            return StatusCode(200);
+        }
+
+        [HttpGet("{id}")]
+        public Domain.VO.ProductFull Get(string id)
+        {
+            bool isParsable = int.TryParse(id, out int number);
+
+            if (!isParsable)
+                throw new ApplicationException("Not a valid id");
+
+            var (res, err) = _productUC.Get(number);
+
+            if (err != null)
+                throw new ApplicationException(err.Message);
+
+            return res;
+        }
+    }
+}
---
+++ Prueba.Delivery/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Prueba.Delivery
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Persistence.Connection.Up();
+            var conn = Persistence.Connection.GetInstance();
+            var productUsecase = new App.Product(new Persistence.SQLite.Product(conn));
+            services.AddSingleton<Domain.Usecase.IProductUsecase>(productUsecase);
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Prueba.Delivery", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Prueba.Delivery v1"));
+            }
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
