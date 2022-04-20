7dd041ab1f25561eab36295e01835390d04f05e9
Walter Leturia <leturia@live.com>
2022-04-20 13:23:13 -0500

    Logger

---
+++ Prueba.Common/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.Common
+{
+    public interface ILog
+    {
+        public void Error(string message);
+        public void Success(string message);
+        public void Message(string message);
+    }
+}
---
+++ Prueba.Common/Log.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.Common
+{
+    public class Log : ILog
+    {
+        private readonly string? pathE;
+
+        public Log()
+        {
+            try
+            {
+                pathE = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Couldn't initialize Logger: " + ex.Message);
+            }
+        }
+
+        public void Error(string message)
+        {
+            WriteMessage("ERROR", message);
+        }
+
+        public void Message(string message)
+        {
+            WriteMessage("INFO", message);
+        }
+
+        public void Success(string message)
+        {
+            WriteMessage("SUCCESS", message);
+        }
+
+        private void WriteMessage(string type, string logMessage)
+        {
+            using StreamWriter txtWriter = File.AppendText(pathE + "\\" + "log.txt");
+
+            try
+            {
+                txtWriter.Write($"\r\n{type} Log: ");
+                txtWriter.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+                txtWriter.WriteLine($"Context: {logMessage}");
+                txtWriter.WriteLine("************************************************************************");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Couldn't write log: " + ex.Message);
+            }
+        }
+    }
+}
